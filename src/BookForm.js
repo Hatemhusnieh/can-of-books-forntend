e3a5fc76162ac709333a0354755c1463ec64c461
Hatemhusnieh <hatemhtm7@gmail.com>
2021-05-18 19:56:57 +0300

    lab13-hatem

---
+++ src/BookForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+export class BookForm extends Component {
+  render() {
+    return (
+      <Form onSubmit={(e)=>{this.props.addBook(e)}}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Book name</Form.Label>
+          <Form.Control onChange={(e)=> this.props.updateBookName(e)} type="text" placeholder="ex: The Lord Of The Rings" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Description</Form.Label>
+          <Form.Control onChange={(e)=> this.props.updateBooDescription(e)} type="text" placeholder="..." />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Status</Form.Label>
+          <Form.Control onChange={(e)=> this.props.updateBookStatus(e)} type="text" placeholder="..." />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+  </Button>
+      </Form>
+    );
+  }
+}
+
+export default BookForm;
+
